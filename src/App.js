3fa8c0c11e9c1104d3593d4df6f8d66f9675b11e
Khrystyna <khrystynavysotska@gmail.com>
2022-06-13 01:17:53 +0300

    Aded route for library report

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import AddReader from "./pages/add_reader/AddReader";
 import AddBook from "./pages/add_book/AddBook";
+import LibraryReport from "./pages/library_report/LibraryReport";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exect path="/" element={<Home />} />
           <Route path="/add-reader" element={<AddReader />} />
           <Route path="/add-book" element={<AddBook />} />
+          <Route path="/library-report" element={<LibraryReport />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
